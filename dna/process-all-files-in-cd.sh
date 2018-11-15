57e1398c0197aa1c6a7aca3b7f418a318850ec54
Tom Atkinson <tom@funk.co.nz>
2018-11-16 00:18:39 +1300

    shell script

--- dna/process-all-files-in-cd.sh
+++ dna/process-all-files-in-cd.sh
@@ -1,1 +1,8 @@
-find -f *.fa *.mfa *.gbk  -exec aminosee -d -f {} \;
+# macos
+# find -f *.fa *.mfa *.gbk  -exec aminosee -d -f {} \;
+
+# linux
+# find -f *.fa *.mfa *.gbk  -exec aminosee -d -f {} \;
+
+
+find . -name "*.fa" "*.mfa" "*.gbk" "*.txt" "*.dna" -type f -print0 | xargs -0 aminosee -d
