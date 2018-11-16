50746e812582ec9cdd6a875d5afc7794f8171dc2
Tom Atkinson <tom@funk.co.nz>
2018-11-17 05:43:07 +1300

    script

--- dna/process-all-files-in-cd.sh
+++ dna/process-all-files-in-cd.sh
@@ -4,8 +4,14 @@
 # linux
 # find -f *.fa *.mfa *.gbk  -exec aminosee -s -v -f {} \;
 
-find . -name "*.fa"  -type f -print0 | xargs -0 aminosee -s -v -z 1200
+find . -name "*.fa"  -type f -print0 | xargs -0 aminosee -s -v
 find . -name "*.mfa" -type f -print0 | xargs -0 aminosee -s -v
 find . -name "*.gbk" -type f -print0 | xargs -0 aminosee -s -v
 find . -name "*.txt" -type f -print0 | xargs -0 aminosee -s -v
 find . -name "*.dna" -type f -print0 | xargs -0 aminosee -s -v
+
+find . -name "*.fa"  -type f -print0 | xargs -0 aminosee -z 4000
+find . -name "*.mfa" -type f -print0 | xargs -0 aminosee -z 4000
+find . -name "*.gbk" -type f -print0 | xargs -0 aminosee -z 4000
+find . -name "*.txt" -type f -print0 | xargs -0 aminosee -z 4000
+find . -name "*.dna" -type f -print0 | xargs -0 aminosee -z 4000
