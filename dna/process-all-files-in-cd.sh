dd1aa31a2422fde515e12f378104f0c6d61659f7
Tom Atkinson <tom@funk.co.nz>
2018-11-17 22:56:25 +1300

    oops

--- dna/process-all-files-in-cd.sh
+++ dna/process-all-files-in-cd.sh
@@ -3,14 +3,22 @@
 
 # linux
 # find -f *.fa *.mfa *.gbk  -exec aminosee -v -d --no-clear --zoom 1 -f {} \;
-aminosee megabase.fa -v -f -z -d 1
-aminosee megabase.fa -v -f -z -d 2
-aminosee megabase.fa -v -f -z -d 3
-aminosee megabase.fa -v -f -z -d 4
-aminosee megabase.fa -v -f -z -d 5
-aminosee megabase.fa -v -f -z -d 6
-aminosee megabase.fa -v -f -z -d 8
-aminosee megabase.fa -v -f -z -d 255
+aminosee megabase.fa -v -f -d -z 1
+aminosee megabase.fa -v -f -d -z 2
+aminosee megabase.fa -v -f -d -z 3
+aminosee megabase.fa -v -f -d -z 4
+aminosee megabase.fa -v -f -d -z 5
+aminosee megabase.fa -v -f -d -z 6
+aminosee megabase.fa -v -f -d -z 8
+aminosee megabase.fa -v -f -d -z 255
+
+
+aminosee z_Brown-Kiwi-aptMan1.fa -v -f -d -z 255
+aminosee z_Brown-Kiwi-aptMan1.fa -v -f -d -z 128
+aminosee z_Brown-Kiwi-aptMan1.fa -v -f -d -z 64
+aminosee z_Brown-Kiwi-aptMan1.fa -v -f -d -z 32
+aminosee z_Brown-Kiwi-aptMan1.fa -v -f -d -z 16
+
 
 
 find . -name "*.fa"  -type f -print0 | xargs -0 aminosee -v -d --no-clear --zoom 1
@@ -19,8 +27,9 @@ find . -name "*.gbk" -type f -print0 | xargs -0 aminosee -v -d --no-clear --zoom
 find . -name "*.txt" -type f -print0 | xargs -0 aminosee -v -d --no-clear --zoom 1
 find . -name "*.dna" -type f -print0 | xargs -0 aminosee -v -d --no-clear --zoom 1
 
-find . -name "*.fa"  -type f -print0 | xargs -0 aminosee -z 4000 --artistic --devmode
-find . -name "*.mfa" -type f -print0 | xargs -0 aminosee -z 4000 --artistic --devmode
-find . -name "*.gbk" -type f -print0 | xargs -0 aminosee -z 4000 --artistic --devmode
-find . -name "*.txt" -type f -print0 | xargs -0 aminosee -z 4000 --artistic --devmode
-find . -name "*.dna" -type f -print0 | xargs -0 aminosee -z 4000 --artistic --devmode
+
+find . -name "*.fa"  -type f -print0 | xargs -0 aminosee -v -d --no-clear --zoom 10
+find . -name "*.mfa" -type f -print0 | xargs -0 aminosee -v -d --no-clear --zoom 10
+find . -name "*.gbk" -type f -print0 | xargs -0 aminosee -v -d --no-clear --zoom 10
+find . -name "*.txt" -type f -print0 | xargs -0 aminosee -v -d --no-clear --zoom 10
+find . -name "*.dna" -type f -print0 | xargs -0 aminosee -v -d --no-clear --zoom 10
