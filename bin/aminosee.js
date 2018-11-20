7f93c2d90590c092ba9b3d566b30a71dd92d5a52
Tom Atkinson <tom@funk.co.nz>
2018-11-21 02:24:14 +1300

    electron desktop app dev begun... at 2am

--- bin/aminosee.js
+++ bin/aminosee.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-require('../')()
+require('../aminosee.js')()
 // require('./aminosee-cli')()
 // require('../aminosee-cli.js')()
--- dna/process-all-files-in-cd.sh
+++ dna/process-all-files-in-cd.sh
@@ -2,8 +2,10 @@
 if [ $(uname)=Darwin ]; then
   echo macos
   find -f *.fa *.mfa *.gbk *.txt -exec aminosee -v -d --no-clear {} \;
+  find -f *.fa *.mfa *.gbk *.txt -exec aminosee --peptide Tryptophan -v -d --no-clear {} \;
   find -f *.fa *.mfa *.gbk *.txt -exec aminosee -w 1 -v -d --no-clear {} \;
   find -f *.fa *.mfa *.gbk *.txt -exec aminosee -w 8 -v -d --no-clear {} \;
+  find -f *.fa *.mfa *.gbk *.txt -exec aminosee --artistic -c 2500  -v -d --no-clear {} \;
 fi
 
 if [ $(uname)=Linux ]; then
@@ -11,4 +13,5 @@ if [ $(uname)=Linux ]; then
   find *.fa *.mfa *.gbk *.txt -exec aminosee -v -d --no-clear {} \;
   find *.fa *.mfa *.gbk *.txt -exec aminosee -w 1 -v -d --no-clear {} \;
   find *.fa *.mfa *.gbk *.txt -exec aminosee -w 8 -v -d --no-clear {} \;
+  find *.fa *.mfa *.gbk *.txt -exec aminosee --artistic -c 2500  -v -d --no-clear {} \;
 fi
