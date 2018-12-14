d58df75eb6e4a8b6ca3ff493ac205dd956796322
Tom Atkinson <tom@funk.co.nz>
2018-12-14 21:23:50 +1300

    batch

--- dna/parallel-starter.sh
+++ dna/parallel-starter.sh
@@ -1,17 +1,24 @@
 
-echo List files without ./ in front
-find . | awk '{sub(/.\//," ")}1'
+echo Starting 8 parallel processes...
 
-nice -n 2  ./batch-peptides.sh $1 $2 $3 $4 $5 $6 &
-echo Starting next quarter in 15 s
-sleep 15
+nice -n 1 ./batch-peptides.sh $1 $2 $3 $4 $5 $6 &
+nice -n 2 ./batch-peptides.sh $1 $2 $3 $4 $5 $6 &
+nice -n 3 ./batch-peptides.sh $1 $2 $3 $4 $5 $6 &
 nice -n 4 ./batch-peptides.sh $1 $2 $3 $4 $5 $6 &
-echo Starting next quarter in 15 s
-sleep 15
+nice -n 5 ./batch-peptides.sh $1 $2 $3 $4 $5 $6 &
 nice -n 6 ./batch-peptides.sh $1 $2 $3 $4 $5 $6 &
-echo Starting next quarter in 15 s
-sleep 15
-nice -n 8 ./batch-process.sh $1 $2 $3 $4 $5 $6
-echo COMPLETED. Starting test in 15 s
-sleep 15
-nice -n 8 ./run-test.sh $1 $2 $3 $4 $5 $6
+nice -n 7 ./batch-peptides.sh $1 $2 $3 $4 $5 $6 &
+nice -n 8 ./batch-peptides.sh $1 $2 $3 $4 $5 $6 &
+nice -n 9 ./batch-peptides.sh $1 $2 $3 $4 $5 $6
+
+
+echo
+echo
+
+
+echo "  MADE IN NEW ZEALAND"
+echo "  ╔═╗┌┬┐┬┌┐┌┌─┐╔═╗┌─┐┌─┐  ╔╦╗╔╗╔╔═╗  ╦  ╦┬┌─┐┬ ┬┌─┐┬─┐"
+echo "  ╠═╣││││││││ │╚═╗├┤ ├┤    ║║║║║╠═╣  ╚╗╔╝│├┤ │││├┤ ├┬┘"
+echo "  ╩ ╩┴ ┴┴┘└┘└─┘╚═╝└─┘└─┘  ═╩╝╝╚╝╩ ╩   ╚╝ ┴└─┘└┴┘└─┘┴└─"
+echo "  by Tom Atkinson            aminosee.funk.nz"
+echo "   ah-mee no-see       'I See It Now - I AminoSee it!' "
