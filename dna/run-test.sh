016dbe782ddef0a850afca9e2b8b8fe2a7689250
Tom Atkinson <tom@funk.co.nz>
2018-12-16 14:44:41 +1300

    tests

--- dna/run-test.sh
+++ dna/run-test.sh
@@ -7,31 +7,26 @@
 
 aminosee_test () {
   echo "doing  $1 $2 $3 $4 $5"
-  nice aminosee $1 $2 $3 $4 $5 --no-updates -m 5 --ratio=gol      --peptide=Ochre
+  nice aminosee $1 $2 $3 $4 $5 --no-updates -m 5 --ratio=gol     --peptide=Ochre
 
   echo "doing  $1 $2 $3 $4 $5"
-  nice aminosee $1 $2 $3 $4 $5 -f --no-updates -m 1 --ratio=sqr    --peptide=Cysteine
+  nice aminosee $1 $2 $3 $4 $5 -f --no-updates -m 1 --ratio=sqr --peptide=Cysteine
 
   echo "doing  $1 $2 $3 $4 $5"
-  nice aminosee $1 $2 $3 $4 $5 --no-updates -m 5 $1 $2 $3 $4 $5    --peptide=Methionine &
-  echo "doing  $1 $2 $3 $4 $5"
-  sleep 1
+  nice aminosee $1 $2 $3 $4 $5 --no-updates -m 5 $1 $2 $3 $4 $5 --peptide=Methionine
 
-  nice aminosee $1 $2 $3 $4 $5 --no-updates -m 10 $1 $2 $3 $4 $5 --peptide=Phenylalanine &
   echo "doing  $1 $2 $3 $4 $5"
-  sleep 1
+  nice aminosee $1 $2 $3 $4 $5 --no-updates -m 10 $1 $2 $3 $4 $5 --peptide=Phenylalanine
 
-  nice aminosee $1 $2 $3 $4 $5 --no-updates -c 500 $1 $2 $3 $4 $5  --peptide=Opal &
   echo "doing  $1 $2 $3 $4 $5"
-  echo "backgrounded... sleeping for 2 seconds"
-  sleep 1
+  nice aminosee $1 $2 $3 $4 $5 --no-updates -c 500 $1 $2 $3 $4 $5  --peptide=Opal
 
+  echo "doing  $1 $2 $3 $4 $5"
   nice aminosee $1 $2 $3 $4 $5 --no-updates --ratio=fix $1 $2 $3 $4 $5 --peptide=Arginine
-  echo "doing  $1 $2 $3 $4 $5"
-  echo "backgrounded... sleeping for 2 seconds"
-  sleep 1
-  echo "resuming..."
 
+  echo "backgrounded... sleeping for 10 seconds"
+  sleep 10
+  echo "resuming..."
 }
 
   echo "                                         =///"
@@ -41,10 +36,10 @@ aminosee_test () {
   echo "                                         =///"
 
 
-nice aminosee megabase.fa megabase.fa Caenorhabditis_elegans-WBcel235-dna-chromosome-V.fa  -f --no-updates -d $1 $2 $3 &
-nice aminosee Caenorhabditis_elegans-WBcel235-dna-chromosome-V.fa megabase.fa  -f --no-updates -d $1 $2 $3 &
-nice aminosee Caenorhabditis_elegans-WBcel235-dna-chromosome-V.fa megabase.fa megabase.fa   -f --no-updates -d --peptide=Amber $1 $2 $3 &
-nice aminosee Caenorhabditis_elegans-WBcel235-dna-chromosome-V.fa megabase.fa megabase.fa   -f --no-updates -d --peptide=Ochre $1 $2 $3 &
+nice aminosee megabase.fa megabase.fa Caenorhabditis_elegans-WBcel235-dna-chromosome-V.fa  -f --no-updates -d $1 $2 $3
+nice aminosee Caenorhabditis_elegans-WBcel235-dna-chromosome-V.fa megabase.fa  -f --no-updates -d $1 $2 $3
+nice aminosee Caenorhabditis_elegans-WBcel235-dna-chromosome-V.fa megabase.fa megabase.fa   -f --no-updates -d --peptide=Amber $1 $2 $3
+nice aminosee Caenorhabditis_elegans-WBcel235-dna-chromosome-V.fa megabase.fa megabase.fa   -f --no-updates -d --peptide=Ochre $1 $2 $3
 
 aminosee megabase.fa AAA-to-TTT-50k.txt megabase.fa AAA-to-TTT-50k.txt megabase.fa AAA-to-TTT-50k.txt megabase.fa -f --peptide=Proline --no-updates -v
 aminosee megabase.fa AAA-to-TTT-50k.txt megabase.fa AAA-to-TTT-50k.txt megabase.fa AAA-to-TTT-50k.txt megabase.fa 37653_ref_Octopus_bimaculoides_v2_0_chrUn.fa -f --peptide=Proline -k -v
@@ -53,26 +48,26 @@ aminosee megabase.fa AAA-to-TTT-50k.txt megabase.fa AAA-to-TTT-50k.txt megabase.
 aminosee megabase.fa AAA-to-TTT-50k.txt Caenorhabditis_elegans-WBcel235-dna-chromosome-V.fa megabase.fa 37653_ref_Octopus_bimaculoides_v2_0_chrUn.fa -f --peptide=Proline -v
 
 aminosee_test $1 $2 $3 $4 $5 --no-updates --peptide=Glutamic
-aminosee_test $1 --no-updates --peptide=Glutamic
-aminosee_test AAA-to-TTT-50k.txt megabase.fa  --peptide=Aspartic --no-updates
-aminosee_test Caenorhabditis_elegans-WBcel235-dna-chromosome-V.fa AAA-to-TTT-50k.txt --peptide=Amber --no-updates
-aminosee_test megabase.fa Caenorhabditis_elegans-WBcel235-dna-chromosome-V.fa AAA-to-TTT-50k.txt --peptide=Tryptophan --no-clear
-aminosee_test * --no-updates -v
+aminosee_test $1 --no-updates --peptide=Glutamic &
+aminosee_test AAA-to-TTT-50k.txt megabase.fa  --peptide=Aspartic --no-updates &
+aminosee_test Caenorhabditis_elegans-WBcel235-dna-chromosome-V.fa AAA-to-TTT-50k.txt --peptide=Amber --no-updates &
+aminosee_test megabase.fa Caenorhabditis_elegans-WBcel235-dna-chromosome-V.fa AAA-to-TTT-50k.txt --peptide=Tryptophan --no-clear &
+aminosee_test * --no-updates -v &
 
 
-aminosee_test             --peptide=Ochre
+aminosee_test      *       --peptide=Ochre
 # aminosee_test            $1 $2 2 $3 $4 --peptide=Glutamic &
 # aminosee_test            $1 $2 3 $3 $4 --peptide=Aspartic &
 # aminosee_test            $1 $2 4 $3 $4 --peptide=Amber &
-aminosee_test             --peptide=Cysteine
+aminosee_test      *       --peptide=Cysteine
 # aminosee_test            $1 $2 6 $3 $4 --peptide=Glycine &
 # aminosee_test_foreground $1 $2 0 $3 $4 --peptide=Alanine
 
-aminosee_test             --peptide=Methionine
+aminosee_test      *       --peptide=Methionine
 # aminosee_test            $1 $2 2 $3 $4 --peptide=Valine &
 # aminosee_test_foreground $1 $2 3 $3 $4 --peptide=Leucine &
 # aminosee_test            $1 $2 4 $3 $4 --peptide=Isoleucine &
-aminosee_test             --peptide=Phenylalanine
+aminosee_test     *       --peptide=Phenylalanine
 # aminosee_test            $1 $2 6 $3 $4 --peptide=Tryptophan
 # aminosee_test            $1 $2 7 $3 $4 --peptide=Serine &
 # aminosee_test_foreground $1 $2 0 $3 $4 --peptide=Threonine
