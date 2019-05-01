46847bc3f152e3f084d27c1cff2d10a9eacb0f17
Tom Atkinson <tom@funk.co.nz>
2019-05-01 17:40:52 +1200

    fix batch size script

--- dna/batch-sizes.sh
+++ dna/batch-sizes.sh
@@ -32,12 +32,12 @@ many_size_hilbert() {
   aminosee_do_foreground 2 $1 -c 3 $2 $3 $4 $5 $6
   aminosee_do 3            $1 -c 5 $2 $3 $4 $5 $6
   aminosee_do 4            $1 -c 6 $2 $3 $4 $5 $6
-  aminosee_do_foreground 5 $1 -c 16 $2 $3 $4 $5 $6
-  # aminosee_do 6            $1 -c 32 $2 $3 $4 $5 $6
-  aminosee_do 7            $1 -c 64 $2 $3 $4 $5 $6
+  aminosee_do_foreground 5 $1 -c 7 $2 $3 $4 $5 $6
+  aminosee_do 6            $1 -c 8 $2 $3 $4 $5 $6
+  aminosee_do 7            $1 -c 100 $2 $3 $4 $5 $6
   # aminosee_do_foreground 8 $1 -c 128 $2 $3 $4 $5 $6
-  aminosee_do 7            $1 -c 256 $2 $3 $4 $5 $6
-  aminosee_do_foreground 8 $1 -c 512 $2 $3 $4 $5 $6
+  aminosee_do 7            $1 -c 500 $2 $3 $4 $5 $6
+  aminosee_do_foreground 8 $1 -c 1000 $2 $3 $4 $5 $6
   # echo "ATTEMPTING TO RENDER AT DIMENSION 9 THIS MAY RUN OUT OF MEMORY"
 
 
