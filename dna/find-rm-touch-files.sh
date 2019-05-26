3f76b99708ce8625a9959a67e36fd602b0f210b1
Tom Atkinson <tom@funk.co.nz>
2019-05-26 21:48:59 +1200

    added script to remove touch files

---
+++ dna/find-rm-touch-files.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+  if [ $(uname)=Darwin ]; then
+    # echo macos
+    DASHF=" -f "
+  fi
+
+  if [ $(uname)=Linux ]; then
+    # echo linux
+    DASHF=" "
+  fi
+
+cwd
+echo "About to delete these touch files:  *_LOCK*.aminosee.touch "
+echo find . -name '*_LOCK*.aminosee.touch'
+
+sleep 1
+find $DASHF . -name '*_LOCK*.aminosee.touch'
+echo "About to delete these touch files:  *_LOCK*.aminosee.touch "
+sleep 5
+echo "Delete these touch files:  *_LOCK*.aminosee.touch "
+find $DASHF . -name '*_LOCK*.aminosee.touch' -exec rm -v {} \;
+sleep 2
