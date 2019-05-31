498af5a8032871ddebd1601663647c07aa6d7cd8
Tom Atkinson <tom@funk.co.nz>
2019-05-31 15:59:18 +1200

    test watcher script now runs updates

--- dna/test-watcher.sh
+++ dna/test-watcher.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
-echo Test watcher
+echo Test watcher starting in two seconds
 echo Test watcher $1 $2 $3
 
 sleep 2
 
 while true; do
-  nice ./run-test.sh --no-updates $1 $2 $3
-  sleep 10
+  nice ./run-test.sh  $1 $2 $3  --no-html --no-image --no-explorer
+  echo running again in 2 mins
+  sleep 30
+  echo 1.5mins
+  sleep 30
+  echo 1 min
+  sleep 30
+  echo 30 secs
+  sleep 25
+  echo running again in 5 seconds
+  echo running again in 5 seconds
+  echo running again in 5 seconds
+  echo running again in 5 seconds
+  echo running again in 5 seconds
+  sleep 5
+  nice git pull
 done
