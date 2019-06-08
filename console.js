c917431bf4e91b26bccfcdb5b18a0928578d6a9d
Tom Atkinson <tom@funk.co.nz>
2019-06-08 23:48:16 +1200

    electron now has a terminal by xterm

--- console.js
+++ console.js
@@ -1,3 +1,10 @@
+import { Terminal } from 'xterm';
+
+let term = new Terminal();
+
+term.open(document.getElementById('xterm-container'));
+
+
 if (typeof console  != "undefined")
   if (typeof console.log != 'undefined')
     console.olog = console.log;
