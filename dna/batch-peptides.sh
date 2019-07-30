3b0975451e702bfabb1170edb45fed306d34cc0e
Tom Atkinson <tom@funk.co.nz>
2019-07-31 06:16:13 +1200

    actual bug fix

--- dna/batch-peptides.sh
+++ dna/batch-peptides.sh
@@ -220,7 +220,7 @@ echo WILL NOW RENDER ALL IN FOLDER
 echo WILL NOW RENDER ALL IN FOLDER
 echo WILL NOW RENDER ALL IN FOLDER
 sleep 5
-# asterix_peptides -c500 --recycle $1 $2 $3 $4 $5 $6 $7
+asterix_peptides $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12
 
 series_peptides $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12 --keyboard
 # series_peptides * $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12 --keyboard
