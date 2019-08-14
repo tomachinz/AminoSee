cbafc2bca50a38e7ffd71eb9758fae7858bddaa7
Tom Atkinson <tom@funk.co.nz>
2019-08-14 19:00:51 +1200

    better update script

--- update.sh
+++ update.sh
@@ -16,5 +16,14 @@ git pull
 git status
 npm i
 npm audit
-npm run test
-npm build-all
+npm run test &
+sleep 10
+echo Building all in 2 minutes
+sleep 60
+echo Building all in 1 minute
+sleep 50
+echo Building all in 10 seconds
+sleep 10
+w
+time npm run build-all
+w
