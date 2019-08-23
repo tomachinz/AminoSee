2546f1ef209c30edbcd6eb01ea2c00c277007395
Tom Atkinson <tom@funk.co.nz>
2019-08-24 06:32:23 +1200

    reduce res

--- dna/batch-peptides.sh
+++ dna/batch-peptides.sh
@@ -2,8 +2,9 @@
 aminosee_do () {
   echo $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12 START RENDER
   nice aminosee --quiet --index --maxpix=4200000 --ratio=sqr --no-image --no-html  $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12
+  nice aminosee -qd --index --maxpix=4200000 --ratio=sqr * $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12
   echo $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12 START RENDER
-  nice aminosee * --quiet --index --maxpix=4200000 --ratio=sqr --no-image --no-html  $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12
+  nice aminosee * -qd --index --maxpix=4200000 --ratio=sqr  $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 $11 $12
 
 }
 
