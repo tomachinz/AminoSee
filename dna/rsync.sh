0467a423ed15a65abf4c0abb5d32410a09b3c30a
Tom Atkinson <tom@funk.co.nz>
2020-07-19 04:50:05 +1200

    finally added a npm ignore file

--- dna/rsync.sh
+++ dna/rsync.sh
@@ -1,5 +1,14 @@
+opn() {
+  echo Open in Finder $OPN
+  ls -la $OPN
+  echo
+  open $OPN
+}
+OPN="$HOME/Desktop/4TB"; opn
+OPN="$HOME/Desktop/HotelVermont"; opn
 
 
+bsync ~/AminoSee_webroot /Volumes/HotelVermont/Users/tom/AminoSee_webroot
 # CLONECOMMAND="/usr/bin/rsync --update --archive --verbose --stats  --dry-run tom@cheese.funk.nz:/home/tom/Sites/AminoSee/dna/AminoSee_webroot/output/  /Users/tom/AminoSee_webroot/output/"
 # DRYCOMMAND="/usr/bin/rsync --update --archive --verbose --stats              tom@cheese.funk.nz:/home/tom/Sites/AminoSee/dna/AminoSee_webroot/output/  /Users/tom/AminoSee_webroot/output/ "
 # WETCOMMAND="/usr/bin/rsync --update --archive --verbose --stats              tom@cheese.funk.nz:/home/tom/Sites/AminoSee/dna/AminoSee_webroot/output/  /Users/tom/AminoSee_webroot/output/ "
@@ -16,12 +25,12 @@
 # CLONECOMMAND="/usr/bin/rsync --archive --verbose --stats  --dry-run  --exclude={'*_linear_*'} tom@cheese.funk.nz:/home/tom/Sites/AminoSee/dna/AminoSee_Output/  /Users/tom/AminoSee_Output/ "
 # WETCOMMAND="/usr/bin/rsync --archive --verbose --stats  --exclude={'*_linear_[ABCDEFGHIJKLMNOPQRSTUVWXYZ]'} /Users/tom/AminoSee_Output/ tom@cheese.funk.nz:/home/tom/Sites/AminoSee/dna/AminoSee_Output/ "
 
-
-DRYCOMMAND="/usr/bin/rsync --update --archive --verbose --stats --delete /Users/tom/AminoSee_webroot/output/  /Users/tom/Dropbox/Sites/funk.co.nz/aminosee/output/ "
-echo ABout to run:
-echo $DRYCOMMAND
-sleep 2
-eval $DRYCOMMAND
-echo
-echo That was from running:
-echo $DRYCOMMAND
+#
+# DRYCOMMAND="/usr/bin/rsync --update --archive --verbose --stats --delete /Users/tom/AminoSee_webroot/output/  /Users/tom/Dropbox/Sites/funk.co.nz/aminosee/output/"
+# echo ABout to run:
+# echo $DRYCOMMAND
+# sleep 2
+# eval $DRYCOMMAND
+# echo
+# echo That was from running:
+# echo $DRYCOMMAND
