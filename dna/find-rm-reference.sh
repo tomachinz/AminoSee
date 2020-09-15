941056b6b188ee0363ac8b996fe2649d4bbc30b2
Tom Atkinson <tom@funk.co.nz>
2020-09-16 04:02:03 +1200

    faster, better, good. powdered toast man.

---
+++ dna/find-rm-reference.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# example: AminoSee_BUSY_LOCK_Pongoabelii._c11__Lysine_fix_sci.txt
+# COMMAND=" find   .  | grep AminoSee_BUSY_LOCK "
+MACCOMMAND="find  ~/AminoSee_webroot/output/  | grep Reference "
+LINUXCOMMAND="find   ~/AminoSee_webroot/output/  | grep Reference "
+PREVIEW="$1 | xargs $XARGPARAM $ ls -laH $ "
+DELETE="$1  | xargs $XARGPARAM $ rm -v $ "
+
+
+  if [ $(uname)=Darwin ]; then
+     echo macos
+     echo $MACCOMMAND | xargs -J $ ls -laH $
+     echo "DELETING THE FILES ABOVE IN 7 SECONDS"
+     echo $MACCOMMAND | xargs -J $ rm -v $
+     sleep 7
+     eval $MACCOMMAND | xargs -J $ rm -v $
+  fi
+
+  if [ $(uname)=Linux ]; then
+    echo linux
+    echo $LINUXCOMMAND | xargs -I $ ls -laH $
+    echo "DELETING THE FILES ABOVE IN 7 SECONDS"
+    echo $LINUXCOMMAND | xargs -I $ rm -v $
+    sleep 7
+    eval $LINUXCOMMAND | xargs -I $ rm -v $
+  fi
